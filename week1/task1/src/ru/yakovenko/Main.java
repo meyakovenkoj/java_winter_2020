7b9c82e1ecc0763c386e97b69574c9a9b8e51956
Ivan Yakovenko <ivan.yakovenko@icloud.com>
2020-02-24 22:24:11 +0300

    Task 1 on week 1

---
+++ week1/task1/src/ru/yakovenko/Main.java
@@ -0,0 +1,49 @@
+package ru.yakovenko;
+
+import java.util.Random;
+
+public class Main {
+
+    private static int[] generateArray(int num) {
+        Random random = new Random(System.currentTimeMillis());
+        return random.ints(num, 0, 100).toArray();
+    }
+
+    private static void replaceMinMax(int[] array){
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, ind_max = 0, ind_min = 0, tmp;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min){
+                min = array[i];
+                ind_min = i;
+            }
+            if (array[i] > max) {
+                max = array[i];
+                ind_max = i;
+            }
+        }
+
+        tmp = array[array.length - 1];
+        array[array.length - 1] = max;
+        array[ind_max] = tmp;
+
+        tmp = array[0];
+        array[0] = min;
+        array[ind_min] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int amount = 15;
+        int[] input_array = generateArray(amount);
+        for (int a: input_array) {
+            System.out.print(a);
+            System.out.print(' ');
+        }
+        System.out.println();
+        replaceMinMax(input_array);
+        for (int a: input_array) {
+            System.out.print(a);
+            System.out.print(' ');
+        }
+    }
+}
